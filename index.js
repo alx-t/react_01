a1a939017ae845ce48066c217389125c366b3590
Alex Tesevich <alex.tesevich@gmail.com>
2018-10-30 15:49:26 +0300

    Add calculations class

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello, world!');
+import { Calculations } from '~/src/Calculations';
+
+console.log('СCalculation 9 & 4');
+let calc = new Calculations(9, 4)
+console.log(`Add = ${calc.getAdd()}`);
+console.log(`Minus = ${calc.getMinus()}`);
+console.log(`Mult = ${calc.getMult()}`);
---
+++ src/Calculations.js
@@ -0,0 +1,16 @@
+export class Calculations {
+  constructor(x, y) {
+    this._x = x;
+    this._y = y;
+  }
+
+  get x() { return this._x; }
+  set x(value) { this._x = value; }
+
+  get y() { return this._y; }
+  set y(value) { this._y = value; }
+
+  getAdd() { return this.x + this.y; }
+  getMinus() { return this.x - this.y; }
+  getMult() { return this.x * this.y; }
+}
